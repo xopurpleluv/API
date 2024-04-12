b22630bff9183dbc9424f19256f888b79cb4f402
We <166823170+xopurpleluv@users.noreply.github.com>
2024-04-12 15:24:34 +0000

    Controller 70%

--- src/Controller/item.controller.js
+++ src/Controller/item.controller.js
@@ -1,14 +1,5 @@
 const ItemArquive = require("../Model/arquive.model");
 
-async function getAllItens(req,res) {
-    try {
-        let allItens = await ItemArquive.find();
-        res.status(200).json(allItens)
-    } catch (error) {
-        res.status(500).json({message: error})
-    }
-}
-
 async function getItem(req,res) {
     let id = req.params.id;
     try {
@@ -19,3 +10,51 @@ async function getItem(req,res) {
     }
 }
 
+async function getAllItens(req,res) {
+    try {
+        let allItens = await ItemArquive.find();
+        res.status(200).json(allItens)
+    } catch (error) {
+        res.status(500).json({message: error})
+    }
+}
+
+async function createItem(req,res) {
+    const newItem = new ItemArquive({
+        item_name: req.body.item_name,
+        item_description: req.body.item_description,
+        item_date_creation: req.body.item_date_creation,
+    });
+
+    try {
+        let nItem = await newItem.save();
+        res.status(200).json(nItem)
+    } catch (error) {
+        res.status(500).json({message: error})
+    }
+}
+
+async function updateItem(req,res) {
+    try {
+        let itemUpdated = await ItemArquive.updateOne(
+            {_id: req.params.id}, {
+            $set: {
+                item_name: req.body.item_name,
+                item_description: req.body.item_description,
+                item_date_creation: req.body.item_date_creation,
+            }
+        })
+        res.status(200).json(itemUpdated)
+    } catch (error) {
+        res.status(500).json({ message: error})
+    }
+}
+
+async function deleteItem(req,res) {
+    let id = req.params.id;
+    try {
+        await ItemArquive.findByIdAndDelete(id)
+    } catch (error) {
+        res.status(500).json({ message: error})
+    }
+}
