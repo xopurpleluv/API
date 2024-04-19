b02c20df8fecac8ea7ca5663cfad1654aeb2cd39
We <166823170+xopurpleluv@users.noreply.github.com>
2024-04-19 14:20:32 +0000

    Updte

--- library-back/src/View/item.view.js
+++ library-back/src/View/item.view.js
@@ -15,5 +15,4 @@ ItemRoute.get("api_v1/route/get_item/:id", getAllItens)
 ItemRoute.post("api_v1/route/create_item", createItem)
 ItemRoute.put("api_v1/route/update_item/:id", updateItem)
 ItemRoute.delete("api_v1/route/delete_item/:id", deleteItem)
-
 module.exports = ItemRoute;
